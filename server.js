f4d6783a45097b6abd694d81f405a7e1f54b2896
Daniel Pereira <danielpaulpereira@protonmail.com>
2018-12-21 00:39:09 -0500

    Complete

--- server.js
+++ server.js
@@ -8,18 +8,19 @@ module.exports = app;
 */
 const PORT = process.env.PORT || 4001;
 
-// Add middleware for handling CORS requests from index.html
+const cors = require('cors');
+app.use(cors());
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-// Add middware for parsing request bodies here:
-
-
-// Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
 
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
-  // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 }
